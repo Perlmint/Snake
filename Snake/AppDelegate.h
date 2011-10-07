0e1f63e7adbe7402766354148ebec76bc5951957
omniavinco <omniavinco@Gyu-sun-ui-MacBook-Pro.local>
2011-10-07 20:30:25 +0900

    Initial Commit

---
+++ Snake/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  Snake
+//
+//  Created by omniavinco on 11. 10. 7..
+//  Copyright __MyCompanyName__ 2011년. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ Snake/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Snake
+//
+//  Created by omniavinco on 11. 10. 7..
+//  Copyright __MyCompanyName__ 2011년. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
